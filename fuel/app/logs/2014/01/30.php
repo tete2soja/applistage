b839e0e4a96117c077480d63fa4f89773b1667f4
edebost <etienne.debost@gmail.com>
2014-01-30 15:20:37 +0100

    ajout d'un model pour admin

--- fuel/app/logs/2014/01/30.php
+++ fuel/app/logs/2014/01/30.php
@@ -1,3 +1,8 @@
 <?php defined('COREPATH') or exit('No direct script access allowed'); ?>
 
 ERROR - 2014-01-30 11:12:21 --> Error - The requested view could not be found: index in /Users/etiennedebost/AppliStage/applistage/fuel/core/classes/view.php on line 388
+ERROR - 2014-01-30 14:12:44 --> Error - The requested view could not be found: index in /Users/etiennedebost/AppliStage/applistage/fuel/core/classes/view.php on line 388
+ERROR - 2014-01-30 14:12:57 --> Compile Error - Cannot redeclare Controller_Admin::action_edit() in /Users/etiennedebost/AppliStage/applistage/fuel/app/classes/controller/admin.php on line 52
+ERROR - 2014-01-30 14:13:38 --> Error - The requested view could not be found: index in /Users/etiennedebost/AppliStage/applistage/fuel/core/classes/view.php on line 388
+ERROR - 2014-01-30 14:15:25 --> Warning - Attempt to assign property of non-object in /Users/etiennedebost/AppliStage/applistage/fuel/app/classes/controller/admin.php on line 13
+ERROR - 2014-01-30 14:15:26 --> Warning - Attempt to assign property of non-object in /Users/etiennedebost/AppliStage/applistage/fuel/app/classes/controller/admin.php on line 13
---
+++ fuel/app/migrations/011_create_admins.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Fuel\Migrations;
+
+class Create_admins
+{
+	public function up()
+	{
+		\DBUtil::create_table('admins', array(
+			'id' => array('constraint' => 11, 'type' => 'int', 'auto_increment' => true, 'unsigned' => true),
+			'title' => array('constraint' => 50, 'type' => 'varchar'),
+			'body' => array('type' => 'text'),
+			'user_id' => array('constraint' => 11, 'type' => 'int'),
+			'login' => array('constraint' => 20, 'type' => 'varchar'),
+			'password' => array('constraint' => 30, 'type' => 'varchar'),
+			'created_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+			'updated_at' => array('constraint' => 11, 'type' => 'int', 'null' => true),
+
+		), array('id'));
+	}
+
+	public function down()
+	{
+		\DBUtil::drop_table('admins');
+	}
+}
