10ff1be46001b6d632d03110f9ea693757f936cf
Nicolas Le Gall <nicolaslegall34@gmail.com>
2014-03-17 17:05:36 +0100

    Mise à jour de la mise en page

--- fuel/app/views/util/connexion.php
+++ fuel/app/views/util/connexion.php
@@ -1,29 +1,37 @@
 <h1>Connexion</h1>
-<form class="form-horizontal" role="form" action="" method="POST">
-  <div class="form-group">
-    <label for="id" class="col-sm-2 control-label">Identifiant</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control" id="id" name="id" placeholder="">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="password" class="col-sm-2 control-label">Mot de passe</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="password" name="password" placeholder="">
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <div class="checkbox">
-        <label>
-          <input type="checkbox" id="remember" name="remember" value="yes">Se souvenir
-        </label>
-      </div>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-    	<button type="submit" name="submit" value="submit" class="btn btn-default">Connexion</button>
-    </div>
-  </div>
-</form>
+<div class="row">
+	<div class="col-md-4">
+	</div>
+	<div class="col-md-4">
+		<form class="form-horizontal" role="form" action="" method="POST">
+			<div class="form-group">
+				<label for="id" class="control-label">Identifiant</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="id" name="id" placeholder="">
+				</div>
+			</div>
+			<div class="form-group">
+				<label for="password" class="control-label">Mot de passe</label>
+				<div class="col-sm-10">
+					<input type="password" class="form-control" id="password" name="password" placeholder="">
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<div class="checkbox">
+						<label>
+							<input type="checkbox" id="remember" name="remember" value="yes">Se souvenir
+						</label>
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" name="submit" value="submit" class="btn btn-default">Connexion</button>
+				</div>
+			</div>
+		</form>
+	</div>
+	<div class="col-md-4">
+	</div>
+</div>
