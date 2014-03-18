bc7de8a61f1272e2c75223abc14e36307d1c3a48
Nicolas Le Gall <nicolaslegall34@gmail.com>
2014-03-18 23:30:23 +0100

    Création des vues pour la gestion des promotions

---
+++ fuel/app/views/admin/promotion/passage.php
@@ -0,0 +1,63 @@
+<style type="text/css">
+	th {
+		text-align: center;
+		padding-left: 10px;
+		padding-right: 10px;
+	}
+</style>
+<h2>Passage des INFO1 en INFO2</h2>
+<br>
+<p>
+	Par défaut, la case validant le changement d'années pour les étudiants est cochée. Vous devez donc seulement décocher les cases concernant les élèves redoublant leur première année.<br />
+	Une seconde case peut-être coché pour les étudiants se réorientant ou démissionnaires afin de les supprimer de la base de données.
+</p>
+<br />
+<?php if ($etudiants): ?>
+<form role="form" method="POST" action="">
+<div class="table-responsive">
+<table class="table-bordered">
+	<thead>
+		<tr>
+			<th>Numéro étudiant</th>
+			<th>Nom</th>
+			<th>Prénom</th>
+			<th>Passage en 2A</th>
+			<th>Supprimer l'étudiant</th>
+		</tr>
+	</thead>
+	<tbody>
+<?php foreach ($etudiants as $etudiant):
+if($etudiant->iut_annee==1) { ?>
+		<tr>
+			<td><?php echo $etudiant->no_etudiant; ?></td>
+			<td><?php echo $etudiant->nom; ?></td>
+			<td><?php echo $etudiant->prenom; ?></td>
+			<td>
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" name="passage[]" value=<?php echo "\"" . $etudiant->no_etudiant . "\""; ?> checked="checked" />
+					</label>
+				</div>
+			</td>
+			<td>
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" name="suppresion[]" value=<?php echo "\"" . $etudiant->no_etudiant . "\""; ?> />
+					</label>
+				</div>
+			</td>
+		</tr>
+<?php } endforeach; ?>	</tbody>
+</table>
+</div>
+
+<?php else: ?>
+<p>Pas d'étudiants dans la base de données actuellement.</p>
+
+<?php endif; ?><p>
+	<br />
+	<div class="row">
+			<div class="col-md-12">
+				<button type="submit" name="submit" value="submit" class="btn btn-default">Valider le passage en deuxième année</button>
+			</div>
+	</div>
