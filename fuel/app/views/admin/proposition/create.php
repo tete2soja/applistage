5e9cde8abc549ae4fc3549924372cd834a231ec6
edebost <etienne.debost@gmail.com>
2014-03-20 13:22:47 +0100

    Suppression partie proposition stage, remplace par stage

--- fuel/app/views/admin/proposition/create.php
+++
@@ -1,7 +0,0 @@
-<h2>New Proposition</h2>
-<br>
-
-<?php echo render('admin/proposition/_form'); ?>
-
-
-<p><?php echo Html::anchor('admin/proposition', 'Back'); ?></p>
--- fuel/app/views/admin/proposition/edit.php
+++
@@ -1,7 +0,0 @@
-<h2>Editing Proposition</h2>
-<br>
-
-<?php echo render('admin/proposition/_form'); ?>
-<p>
-	<?php echo Html::anchor('admin/proposition/view/'.$proposition->id, 'View'); ?> |
-	<?php echo Html::anchor('admin/proposition', 'Back'); ?></p>
