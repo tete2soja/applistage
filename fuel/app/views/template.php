f98df16f10527875aecd4fd76200974aee15d5bb
Nicolas Le Gall <nicolaslegall34@gmail.com>
2014-03-31 17:35:08 +0200

    Ajout favicon

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -19,6 +19,7 @@
 				margin-top: 10px;
 			}
 		</style>
+		<link rel="icon" href="assets/img/favicone.jpg" />
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
 		<script src="//code.jquery.com/jquery-1.11.0.js"></script>
 		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
