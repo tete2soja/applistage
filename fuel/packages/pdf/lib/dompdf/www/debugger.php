7e8c4689df877414781994b22884afe2765c9cd9
Nicolas Le Gall <nicolas.legall@synoptik-labs.com>
2024-02-27 11:33:40 +0900

    Fix dompdf jquery

--- fuel/packages/pdf/lib/dompdf/www/debugger.php
+++ fuel/packages/pdf/lib/dompdf/www/debugger.php
@@ -6,7 +6,7 @@ $files = glob("test/*.{html,htm,php}", GLOB_BRACE);
 <html lang="en">
 <head>
   <title>dompdf debugger</title>
-  <script type="text/javascript" src="jquery-1.4.2.js"></script>
+  <script type="text/javascript" src="jquery-1.12.4.js"></script>
 	
 	<script type="text/javascript">
 		function updateAddress(){
